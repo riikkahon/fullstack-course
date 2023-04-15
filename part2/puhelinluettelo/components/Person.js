29a68b9a49d9ff2ba68c33e8c6cbabe39cecf4d7
riikkahon <130756047+riikkahon@users.noreply.github.com>
2023-04-15 19:42:53 +0300

    Delete part2/puhelinluettelo/components directory

--- part2/puhelinluettelo/components/Person.js
+++
@@ -1,8 +0,0 @@
-const Person = ({persons, handleDelete}) => {
-    return(
-      <p>{persons.name} {persons.number} <button onClick={() => handleDelete(persons.id)}>delete</button></p>
-    )
-    
-  }
-
-  export default Person
