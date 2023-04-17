a721a17b74910a6404e82ab2f09c33f864a429fe
riikkahon <130756047+riikkahon@users.noreply.github.com>
2023-04-17 09:30:28 +0300

    Add files via upload

---
+++ part3/puhelinluettelo-frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import axios from 'axios'
+import './index.css'
+
+import App from './App'
+
+axios.get('http://localhost:3001/api/persons').then(response => {
+  const persons = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App persons={persons}/>)
+})
