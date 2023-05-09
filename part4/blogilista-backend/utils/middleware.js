8b458c2b6d061d422cbe7f3790e60273252e3965
riikkahon <130756047+riikkahon@users.noreply.github.com>
2023-05-09 09:15:05 +0300

    Add files via upload

---
+++ part4/blogilista-backend/utils/middleware.js
@@ -0,0 +1,63 @@
+const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.body)
+  logger.info('---')
+  next()
+}
+
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+  
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
+  }
+  
+    
+  next(error)
+}
+
+
+const tokenExtractor = async (request, response, next) => {
+  const authorization = await request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+
+  next()
+}
+
+
+const userValidator = (request, response, next) => {
+  const token = request.token
+  if (!token) {
+    return response.status(401).json({ error: 'token is missing' })
+  }
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token is invalid' })
+  }
+  next()
+}
+  
+  
+module.exports = {
+  requestLogger,
+  errorHandler,
+  tokenExtractor,
+  userValidator
+}
